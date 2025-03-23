129107ac65ae1f14b6907db7b1b03ac71ebeacef
AlexSimionGeorge <alexsimiongeorge@gmail.com>
2025-03-23 22:01:45 +0100

    fixed refactoring

--- apps/owner/services.py
+++ apps/owner/services.py
@@ -3,12 +3,12 @@ from .repositories import OwnerRepository
 class OwnerService:
     @staticmethod
     def list_owners():
-        return OwnerRepository.get_all_owners()
+        return OwnerRepository.get_all()
 
     @staticmethod
     def create_owner(data):
-        return OwnerRepository.create_owner(data)
+        return OwnerRepository.create(data)
 
     @staticmethod
     def delete_owner(owner_id):
-        return OwnerRepository.delete_owner(owner_id)
+        return OwnerRepository.delete(owner_id)
--- apps/veterinarian/services.py
+++ apps/veterinarian/services.py
@@ -3,12 +3,12 @@ from .repositories import VeterinarianRepository
 class VeterinarianService:
     @staticmethod
     def list_owners():
-        return VeterinarianRepository.get_all_veterinarians()
+        return VeterinarianRepository.get_all()
 
     @staticmethod
     def create_owner(data):
-        return VeterinarianRepository.create_veterinarian(data)
+        return VeterinarianRepository.create(data)
 
     @staticmethod
     def delete_owner(owner_id):
-        return VeterinarianRepository.delete_veterinarian(owner_id)
+        return VeterinarianRepository.delete(owner_id)
