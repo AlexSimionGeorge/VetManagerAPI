6131ffb083d0fb5944bb170cf40c285eaf1adbed
AlexSimionGeorge <alexsimiongeorge@gmail.com>
2025-03-24 23:47:02 +0100

    changed sex db field size

--- apps/animal/models.py
+++ apps/animal/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 class Animal(models.Model):
     name = models.CharField(max_length=255)
-    sex = models.CharField(max_length=255)
+
+    sex = models.CharField(max_length=1, choices=[
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]) #(value for db, prompt ui)
+
     castrated = models.BooleanField()
     birth_date = models.DateField()
     has_microchip = models.BooleanField()
     microchip_id = models.CharField(max_length=255)
-
 
     class Meta:
         db_table = "animal"
